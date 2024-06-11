b956b7e87cb0c0befe944b69986b297f8be2c737
ASHISH MOHANTY <ashishmohaty10@gmail.com>
2024-06-12 01:00:14 +0530

    done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
-import Image from "next/image";
-import HomeBGDesktop from "../../public/home/background-home-desktop.jpg";
-import HomeBGTablet from "../../public/home/background-home-tablet.jpg";
-import HomeBGMobile from "../../public/home/background-home-mobile.jpg";
+import { BG } from "@/components/homeBG";
+
 export default function Home() {
   return (
     <div>
@@ -36,26 +34,3 @@ export default function Home() {
 }
 
 // Home page backgorund image
-const BG = () => {
-  return (
-    <div className="absolute top-0 left-0 w-full -z-20">
-      <Image
-        src={HomeBGMobile}
-        alt="homepage background for tablet"
-        className="min-h-screen object-cover md:hidden w-full"
-      />
-
-      <Image
-        src={HomeBGTablet}
-        alt="homepage background for tablet"
-        className="hidden md:block min-h-screen object-cover xl:hidden w-full"
-      />
-
-      <Image
-        src={HomeBGDesktop}
-        alt="homepage background"
-        className="hidden max-h-screen w-full object-cover xl:block"
-      />
-    </div>
-  );
-};
